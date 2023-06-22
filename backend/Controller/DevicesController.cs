98fcf07ab0dcde89af9045a34efb344a2cad7bbb
RiyazurRazak <riyazurrazaknoushath786@gmail.com>
2023-06-22 19:25:14 +0530

    Add: Devices Controller Added (Empty)

---
+++ backend/Controller/DevicesController.cs
@@ -0,0 +1,22 @@
+﻿using backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger _logger;
+
+
+        public DevicesController(ApplicationDbContext dbContext, ILogger<DevicesController> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+   
+    }
+}
