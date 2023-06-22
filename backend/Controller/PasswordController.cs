663fc53c0865a0ce2051a9f614f7785befc6ad18
RiyazurRazak <riyazurrazaknoushath786@gmail.com>
2023-06-22 20:05:47 +0530

    Add: Password controller added (Empty)

---
+++ backend/Controller/PasswordController.cs
@@ -0,0 +1,21 @@
+﻿using backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controller
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class PasswordController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger _logger;
+       public PasswordController(ApplicationDbContext dbContext, ILogger<PasswordController> logger) 
+       {
+
+            _dbContext = dbContext;
+            _logger = logger;
+       }
+
+    }
+}
