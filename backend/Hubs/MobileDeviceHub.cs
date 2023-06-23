071adc0edd295402df5802d40dd8c60a9918f0eb
RiyazurRazak <riyazurrazaknoushath786@gmail.com>
2023-06-23 12:16:29 +0530

    Add: signalr mobile device hub added

---
+++ backend/Hubs/MobileDeviceHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace backend.Hubs
+{
+    public class MobileDeviceHub: Hub
+    {
+        private readonly ILogger<MobileDeviceHub> _logger;
+
+        public MobileDeviceHub(ILogger<MobileDeviceHub> logger)
+        {
+            _logger = logger;
+        }
+        public override Task OnConnectedAsync()
+        {
+            var ConnectionId = Context.ConnectionId;
+            var ExtensionId = Context.GetHttpContext().Request.Query["ext"];
+            _logger.LogInformation("Browser extension connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            var ConnectionId = Context.ConnectionId;
+            var ExtensionId = Context.GetHttpContext().Request.Query["ext"];
+            _logger.LogInformation("Browser extension disconnected");
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
